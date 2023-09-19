7d52b677bc6384c210a4477429736c1ed41b78f3
CO-UnknowKE <none>
2023-09-19 23:05:19 +0800

    删除无用判断

--- src/api/api.ts
+++ src/api/api.ts
@@ -18,7 +18,7 @@ export const getChatCompletion = async (
   if (isAzureEndpoint(endpoint) && apiKey) {
     headers['api-key'] = apiKey;
 
-    const model = config.model === 'gpt-3.5-turbo' ? 'gpt-35-turbo' : config.model === 'gpt-3.5-turbo-16k' ? 'gpt-35-turbo-16k' : config.model;
+    const model = config.model;
 
     const apiVersion = '2023-03-15-preview';
 
